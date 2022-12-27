c932081a239f65f55782aebe850101119ab26528
Sina Bayandorian <sina.sina.bayan@protonmail.com>
2022-12-27 20:50:42 +0330

    favicon changed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,18 @@ export default function App({ Component, pageProps }: AppProps) {
           name='description'
           content='a web-app for you to keep track of prices in the crypto world'
         />
-        <link rel='icon' href='/favicon.ico' />
+        <link
+          rel='icon'
+          type='image/svg+xml'
+          href='/bitcoin-light-theme.svg'
+          media='(prefers-color-scheme: light)'
+        />
+        <link
+          rel='icon'
+          type='image/svg+xml'
+          href='/bitcoin-dark-theme.svg'
+          media='(prefers-color-scheme: dark)'
+        />
       </Head>
       <Component {...pageProps} />
     </>
